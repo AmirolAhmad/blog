1204082fcbb07f6af42ad3babefe0858a5d54e31
Amirol Ahmad <9w2pdf@gmail.com>
2014-05-06 15:45:04 +0800

    add sorting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
 
 	def index
 		#@articles = Article.all
-		@articles = Article.paginate(:page => params[:page], :per_page => 5)
+		@articles = Article.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
 	end
 
 	def new
