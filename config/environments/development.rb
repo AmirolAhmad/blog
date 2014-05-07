06b8bd225673366ed6a6d6c7f62f248016b2a3d3
Amirol Ahmad <9w2pdf@gmail.com>
2014-05-07 23:57:49 +0800

    set default assets server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # config.action_controller.asset_host = "https://s3.amazonaws.com/mramirol"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
