be92e913bfb7dace02b2cc7b0b7b0d50551f64f0
AmirolAhmad <9w2pdf@gmail.com>
2014-05-12 16:00:25 +0800

    fixed the routes for v4.1.1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles
 
-  match 'about' => 'welcome#about', via: 'get'
+  #match 'about' => 'welcome#about', via: 'get'
+
+  get 'welcome/about', controller: 'welcome', action: 'about'
   
   root to: 'articles#index'
 
