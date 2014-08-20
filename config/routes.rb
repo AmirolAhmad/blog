9182120acf7d5aea1ee94981a5c936632e3be8dd
AmirolAhmad <9w2pdf@gmail.com>
2014-08-20 10:59:42 +0800

    friendly url for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy'
+    get '/login' => 'devise/sessions#new'
+  end
   resources :articles
 
   match 'about' => 'welcome#about', via: 'get'
