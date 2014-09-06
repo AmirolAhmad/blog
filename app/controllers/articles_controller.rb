883302afdb10440950b7425d962217c80f2d44f9
AmirolAhmad <9w2pdf@gmail.com>
2014-09-06 13:35:57 +0800

    limit to 7 article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,10 @@ class ArticlesController < ApplicationController
 
 	def index
 		if params[:q]
-		    @articles = Article.search(params[:q].to_s).order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+		    @articles = Article.search(params[:q].to_s).order("created_at DESC").paginate(:page => params[:page], :per_page => 7)
 		    @articles_archieve = Article.all.order("created_at DESC").limit(10)
 		else
-		    @articles = Article.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+		    @articles = Article.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 7)
 		    @articles_archieve = Article.all.order("created_at DESC").limit(10)
 		end
 		@feeds = FeedEntry.all.order("published_at DESC").limit(10)
