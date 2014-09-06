1967d9c967a9aa5ab26ee1c4524fe7bc665b42b3
AmirolAhmad <9w2pdf@gmail.com>
2014-09-06 13:54:56 +0800

    Random Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,10 @@ class ArticlesController < ApplicationController
 	def index
 		if params[:q]
 		    @articles = Article.search(params[:q].to_s).order("created_at DESC").paginate(:page => params[:page], :per_page => 7)
-		    @articles_archieve = Article.all.order("created_at DESC").limit(10)
+		    @articles_archieve = Article.all.order("RAND()").limit(10)
 		else
 		    @articles = Article.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 7)
-		    @articles_archieve = Article.all.order("created_at DESC").limit(10)
+		    @articles_archieve = Article.all.order("RAND()").limit(10)
 		end
 		@feeds = FeedEntry.all.order("published_at DESC").limit(10)
 	end
